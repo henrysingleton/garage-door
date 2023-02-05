0c7b3f8f10e0934c10f101c55232f57d0b922040
Henry Singleton <henry@henrysingleton.com>
2023-02-05 14:28:19 +1100

    rewrite to be more simple

--- src/garage_door/controller.py
+++ src/garage_door/controller.py
@@ -1,8 +1,7 @@
 import time
 from enum import Enum
-from typing import Optional, Any
-from pydantic import BaseModel
 from gpiozero import LED, Button
+
 
 class State(Enum):
     OPEN = "Open"
@@ -12,233 +11,94 @@ class State(Enum):
     STOPPED = "Stopped"
 
 
-SensorData = tuple[int, int]
-
-
-class StateResolutionError(RuntimeError):
-    pass
-
-
-class StateUpdateDebounceException(StateResolutionError):
-    pass
-
-
-class StateDefinition(BaseModel):
-    """Pydantic method describing a door state"""
-    sensor: SensorData  # top, bottom
-    last: Optional[State]
-
-
-# Define some door states, not sure if these would actually get used
-states: dict[State, StateDefinition] = {
-    State.OPEN: StateDefinition(
-        sensor=(1, 0),
-        last=None
-    ),
-    State.CLOSED: StateDefinition(
-        sensor=(0, 1),
-        last=None
-    ),
-    State.OPENING: StateDefinition(
-        sensor=(0, 0),
-        last=State.CLOSED
-    ),
-    State.CLOSING: StateDefinition(
-        sensor=(0, 0),
-        last=State.OPEN
-    ),
-}
-
-
 class DoorController:
-    STATE_DEBOUNCE: float = 0.3  # seconds
-    DOOR_PIN = 23  # pin where door is connected
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, state: State) -> None:
+        print(f"Set state: {state}")
+        if state != self.state:
+            self._state = state
+
+    # Physical configuration
+    DOOR_PIN = 23  # pin where door relay is connected
     TOP_SENSOR_PIN = 25
     BOTTOM_SENSOR_PIN = 24
 
     def __init__(self) -> None:
-        self.state: State = State.CLOSED
-        self.lastState: State = State.CLOSED
-        self.lastUpdateTime: float = time.time()
-
         self.door_pin = LED(DoorController.DOOR_PIN)
         self.top_sensor = Button(DoorController.TOP_SENSOR_PIN, hold_time=2)
         self.bottom_sensor = Button(DoorController.BOTTOM_SENSOR_PIN, hold_time=2)
 
-        self.top_sensor.when_held = self.update_state
-        self.top_sensor.when_deactivated = self.update_state
-        self.bottom_sensor.when_held = self.update_state
-        self.bottom_sensor.when_deactivated = self.update_state
+        # Set the initial sensor state
+        self.set_state_from_sensors()
 
-    @staticmethod
-    def resolve_state(
-        sensor: SensorData,
-        current: State,
-        last: State,
-        delta: float
-    ) -> State:
-        """Resolves the current state from the passed information"""
-        print(
-            f"\n Resolving state with: \n"
-            f" sensor: {sensor}\n"
-            f" current: {current.value}\n"
-            f" last: {last.value}\n"
-            f" delta: {delta}"
-        )
+        # Add handlers to update the state when the sensors change
+        self.top_sensor.when_held = self.handle_open
+        self.top_sensor.when_deactivated = self.handle_closing
+        self.bottom_sensor.when_held = self.handle_closed
+        self.bottom_sensor.when_deactivated = self.handle_opening
 
-        if delta < DoorController.STATE_DEBOUNCE:
-            print(f"Refusing to update, last state change was "
-                  f"{delta} seconds ago")
-            raise StateUpdateDebounceException(delta)
+    def set_state_from_sensors(self):
+        # Set the current state
+        if self.top_sensor.is_held:
+            self.state = State.OPEN
+        elif self.bottom_sensor.is_held:
+            self.state = State.CLOSED
 
-        state_by_sensor_data = [state for state, definition in states.items() if definition.sensor == sensor]
+    # Handle sensor transitions
+    def handle_open(self):
+        self.state = State.OPEN
 
-        # If we got a direct match on OPEN or CLOSED, just return it, we are very confident about the state.
-        if len(state_by_sensor_data) == 1:
-            print(
-                f"resolved to {state_by_sensor_data[0].value} via "
-                f"direct sensor state match"
-            )
-            return state_by_sensor_data[0]
+    def handle_closing(self):
+        self.state = State.CLOSING
 
+    def handle_closed(self):
+        self.state = State.CLOSED
 
-        # If its a new state, decide if its opening or closing based on the last known state.
-        possible_states_from_last_known_state = [state for state, definition in states.items() if state in state_by_sensor_data and definition.last == current]
+    def handle_opening(self):
+        self.state = State.OPENING
 
-        if len(possible_states_from_last_known_state) == 1:
-            print(
-                f"resolved to {possible_states_from_last_known_state[0].value}"
-                f" as we have transitioned from {current}"
-            )
-            return possible_states_from_last_known_state[0]
-
-        # If the state was opening or closing and current is the same as last, just assume its still in the same state. Could put a little timeout here.
-        if last == current and states[current].last is not None:
-            print(
-                f"resolved to {current} as it's the same as the last opening "
-                f"or closing state ",
-            )
-            return current
-
-        raise StateResolutionError(
-            "Could not resolve state, no matches from state definition"
-        )
-
-    def set_state(self, state: State) -> None:
-        self.lastState = self.state
-        self.state = state
-        self.lastUpdateTime = time.time()
-
-    def get_state(self) -> State:
-        return self.state
-
-    def open(self) -> State:
-        self.update_state()
-        print(f"Doing opening...")
-        if self.state == State.OPEN:
-            print(f"Not doing anything, door already open...")
-            return self.state
-
-        if self.state == State.OPENING:
-            print(f"Not doing anything, door already opening")
-            return self.state
-
-        if self.state == State.STOPPED:
-            # Not sure if we'll ever know its stopped
-            if self.lastState == State.OPENING:
-                self.press_button()  # closing
-                self.press_button()  # stopped
-                self.press_button()  # opening
-                return State.OPENING
-
-            if self.lastState == State.CLOSING:
-                self.press_button()  # stopped
-                self.press_button()  # opening
-                return State.OPENING
-
-            raise RuntimeError(
-                "Not sure what to do from unknown state when opening")
-
-        if self.state == State.CLOSING:
-            self.press_button()  # stopped
-            self.press_button()  # opening
-            return State.OPENING
-
-        if self.state == State.CLOSED:
-            self.press_button()  # opening
-            return State.OPENING
-
-    def close(self) -> State:
-        self.update_state()
-        if self.state == State.CLOSED:
-            print(f"Not doing anything, door already closed...")
-            return self.state
-
-        if self.state == State.CLOSING:
-            print(f"Not doing anything, door already closing")
-            return self.state
-
-        if self.state == State.STOPPED:
-            # Not sure if we'll ever know its stopped
-            if self.lastState == State.CLOSING:
-                self.press_button()  # opening
-                self.press_button()  # stopped
-                self.press_button()  # closing
-                return State.CLOSING
-
-            if self.lastState == State.OPENING:
-                self.press_button()  # stopped
-                self.press_button()  # closing
-                return State.CLOSING
-
-            raise RuntimeError("Not sure what to do from unknown state when closing")
-
-        if self.state == State.OPENING:
-            self.press_button()  # stopped
-            self.press_button()  # closing
-            return State.CLOSING
-
-        if self.state == State.OPEN:
-            self.press_button()  # closing
-            return State.CLOSING
-
+    # Handle open/close requests
     def press_button(self):
         self.door_pin.on()
         time.sleep(0.5)
         self.door_pin.off()
         time.sleep(0.5)
 
-    def update_state(self, sensor: Optional[Any] = None) -> State:
-        """Given the following:
-        - Sensor 1 & 2
-        - Current state
-        - Last state
-        - Last state update time
+    def open(self):
+        if self.state == State.OPEN:
+            print(f"Not doing anything, door already open")
 
-        Resolve the new state, set it, and return it"""
+        if self.state == State.OPENING:
+            print(f"Not doing anything, door already opening")
 
-        if sensor:
-            print(f"Updating state from sensor {sensor} change")
+        if self.state == State.CLOSING:
+            self.press_button()
+            self.state = State.STOPPED
+            self.press_button()
+            self.state = State.OPENING
 
-        print(f"Updating state. Resolving first...")
+        if self.state == State.CLOSED:
+            self.press_button()  # opening
+            # Let sensor changes take care of changing the state to opening
+            # self.state = State.OPENING
 
-        new_state = self.resolve_state(
-            self.get_sensor_data(),
-            self.state,
-            self.lastState,
-            time.time() - self.lastUpdateTime
-        )
+    def close(self):
+        if self.state == State.CLOSED or self.state == State.CLOSING:
+            print(f"Not doing anything, door is {self.state}")
 
-        print(f"Checking time diff")
+        if self.state == State.OPENING:
+            self.press_button()
+            self.state = State.STOPPED
+            self.press_button()
+            self.state = State.CLOSING
 
-        if new_state != self.lastState:
-            print(f"Saving state")
-            self.set_state(new_state)
-            # TODO: make request to homebridge to let them know the state
-            #  changed.
-        print(f"Returning")
+        if self.state == State.OPEN:
+            self.press_button()  # closing
+            # Let sensor changes take care of changing the state to opening
+            # self.state = State.CLOSING
 
-        return new_state
 
-    def get_sensor_data(self):
-        return self.top_sensor.value, self.bottom_sensor.value
--- src/garage_door/web_server.py
+++ src/garage_door/web_server.py
@@ -6,8 +6,8 @@ HOST_NAME = "192.168.0.202"
 SERVER_PORT = 8080
 WEBHOOK_ADDRESS = "http://192.168.0.6:8089/bay1"
 
-
 controller = DoorController()
+
 
 class WebServer(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -16,37 +16,29 @@ class WebServer(BaseHTTPRequestHandler):
         self.end_headers()
 
         if self.path == "/state":
-            state = controller.get_state()
-            print(f"Sending state {state.value}")
-            self.wfile.write(bytes(str(state.value), "utf-8"))
-
-        if self.path == "/reset/closed":
-            controller.set_state(State.CLOSED)
-        if self.path == "/reset/open":
-            controller.set_state(State.OPEN)
+            self.wfile.write(bytes(str(controller.state), "utf-8"))
 
     def do_PUT(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
 
-        state = controller.get_state()
-
-        print(
-            f"Current state: {state.value}, last updated {controller.lastUpdateTime}"
-        )
-
-        print(self.path)
-
         if self.path == "/open":
             controller.open()
         if self.path == "/close":
             controller.close()
+        if self.path == "/reset":
+            controller.set_state_from_sensors()
+        if self.path == "/reset/closed":
+            controller.state = State.CLOSED
+        if self.path == "/reset/open":
+            controller.state = State.OPEN
 
 
 if __name__ == "__main__":
     webServer = HTTPServer((HOST_NAME, SERVER_PORT), WebServer)
     print("Server started http://%s:%s" % (HOST_NAME, SERVER_PORT))
+    print(controller.state)
 
     try:
         webServer.serve_forever()
